6fdcd3b0ff0e11d5e25d0c2504908010c671c8b8
邢浩东 <xinghaodong@pubmi.org>
2026-04-27 16:34:55 +0800

    feat: add langchain

--- app/bailian/bailian.py
+++ app/bailian/bailian.py
@@ -2,14 +2,14 @@ import os
 from openai import OpenAI
 
 client = OpenAI(
-    # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx"
-    api_key=os.getenv("DASHSCOPE_API_KEY"),
+    # 直接写你的API Key，不要用 os.getenv()
+    api_key="sk-ffabef30d4b145deb70463a0105d6f3c",
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
 completion = client.chat.completions.create(
     # 模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
-    model="qwen-plus",
+    model="qwen3.6-plus",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "你是谁？"},
