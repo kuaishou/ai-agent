2c6a3d7541455ba7556399df637445f35364088e
邢浩东 <xinghaodong@pubmi.org>
2026-04-28 16:50:49 +0800

    feat: add langchain

--- app/bailian/common.py
+++ app/bailian/common.py
@@ -4,6 +4,7 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.prompts import ChatMessagePromptTemplate
 from langchain_core.prompts import FewShotPromptTemplate
+from pydantic import BaseModel, Field
 llm = ChatOpenAI(
     model="qwen3.6-plus",
     # api_key="sk-ffabef30d4b145deb70463a0105d6f3c",
@@ -30,3 +31,19 @@ chat_prompt_template = ChatPromptTemplate.from_messages([
 ("system", "你是一位{role}专家，擅长回答{domain}领域的问题"),
 ("user", "用户问题{question}")
 ])
+
+class AddInputArgs(BaseModel):
+a: int = Field(description="first number")
+b: int = Field(description="second number")
+@tool(
+    description="add two numbers",
+    args_schema=AddInputArgs,
+    return_direct=True,
+)
+def add(a, b):
+    """add two numbers"""
+    return a + b
+def create_calc_tools():
+    return [add]
+    
+calc_tools = create_calc_tools()
